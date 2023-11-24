ff7bf16983f5e97817386c16a2d49a68206581fd
Abbas Valliani <abbasvalliani@gmail.com>
2023-11-24 16:05:16 -0500

    Added more NLP test cases

--- apps/ml-demo-api-e2e/src/ml-demo-api/ml-demo-api.spec.ts
+++ apps/ml-demo-api-e2e/src/ml-demo-api/ml-demo-api.spec.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import {NLPRequestDto} from "../../../ml-demo-api/src/app/types/NLPTypes";
 import {NLPLabel, NLPResult} from "@safekids-ai/nlp-js-types";
+import {int} from "@techstark/opencv-js";
 
 describe('GET /api/v1/hello', () => {
   it('ping', async () => {
@@ -19,6 +20,7 @@ describe('POST /api/v1/find-hate', () => {
 
     expect(res.status).toBeGreaterThanOrEqual(200);
     expect(res.status).toBeLessThan(300);
+
     const expected: NLPResult = {flag: false, label: NLPLabel.Clean, flaggedText: ""}
     expect(res.data).toEqual(expected);
   });
@@ -33,5 +35,73 @@ describe('POST /api/v1/find-hate', () => {
 
     const expected: NLPResult = {flag: true, label: NLPLabel.HateBullying, flaggedText: "but you're also an asshole"}
     expect(res.data).toEqual(expected);
+  });
+});
+
+describe('POST /api/v1/classify-text', () => {
+  it('should be clean', async () => {
+    const res = await axios.post(`api/v1/classify-text`, {
+      message : "you are amazing."
+    } as NLPRequestDto);
+
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+
+    expect(res.data).toEqual(NLPLabel.Clean);
+  });
+
+  it('should be hate', async () => {
+    const res = await axios.post(`api/v1/classify-text`, {
+      message : "you are an asshole."
+    } as NLPRequestDto);
+
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+
+    expect(res.data).toEqual(NLPLabel.HateBullying);
+  });
+
+  it('should be adult', async () => {
+    const res = await axios.post(`api/v1/classify-text`, {
+      message : "find adult sex links videos"
+    } as NLPRequestDto);
+
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+
+    expect(res.data).toEqual(NLPLabel.Porn);
+  });
+
+  it('should be proxy', async () => {
+    const res = await axios.post(`api/v1/classify-text`, {
+      message : "find proxy websites to get around filters"
+    } as NLPRequestDto);
+
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+
+    expect(res.data).toEqual(NLPLabel.Proxy);
+  });
+
+  it('should be weapons', async () => {
+    const res = await axios.post(`api/v1/classify-text`, {
+      message : "where do I buy a gun"
+    } as NLPRequestDto);
+
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+
+    expect(res.data).toEqual(NLPLabel.Weapons);
+  });
+
+  it('should be self harm', async () => {
+    const res = await axios.post(`api/v1/classify-text`, {
+      message : "how do i hurt myself"
+    } as NLPRequestDto);
+
+    expect(res.status).toBeGreaterThanOrEqual(200);
+    expect(res.status).toBeLessThan(300);
+
+    expect(res.data).toEqual(NLPLabel.SelfHarm);
   });
 });
