73e48dfa6c81a1c48fd12d882682d7cf86f34b44
Zohran valliani <zohranvalliani@Zohrans-MacBook-Air.local>
2023-11-26 16:06:34 -0500

    Initial commit of UI

--- apps/ml-api-web/src/main.tsx
+++ apps/ml-api-web/src/main.tsx
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-import App from './app/app';
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
