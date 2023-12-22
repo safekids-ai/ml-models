d6c3a2daac025ef4276813d151fc6f26f7fd20fc
Abbas Valliani <abbasvalliani@gmail.com>
2023-12-22 14:56:03 -0500

    Homepage changes

--- apps/ml-api-web/components/pages/homepage/Hero.tsx
+++ apps/ml-api-web/components/pages/homepage/Hero.tsx
@@ -129,90 +129,11 @@ export const Code = () => {
     </div>
   );
 };
-export function Test() {
-  return (
-    <div className="overflow-hidden bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div>
-            <div className="relative isolate pt-14">
-              <div
-                className="absolute inset-x-0 -top-40 -z-10 transform-gpu blur-3xl sm:-top-80 overflow-x-hidden"
-                aria-hidden="true"
-              >
-                <div
-                  className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-                  style={{
-                    clipPath:
-                      'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                  }}
-                />
-              </div>
-              <div className="pt-24 sm:pt-32">
-                <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                  <div className="mx-auto max-w-2xl text-center">
-                    <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl break-words overflow-hidden dark:text-white">
-                      Models to help detect NSFW images and text
-                    </h1>
-                    <p className="mt-6 text-lg leading-8 text-gray-600 break-words overflow-hidden dark:text-white">
-                      Use these models that can run on the browser today! It is
-                      as simple as 10 lines of code.
-                    </p>
-                    <div className="mt-10 flex items-center justify-center gap-x-6">
-                      <Link
-                        href="/docs"
-                        className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-400"
-                      >
-                        Get started
-                      </Link>
-                      <a
-                        href="#"
-                        className="text-sm font-semibold leading-6 text-gray-900 dark:text-white"
-                      >
-                        Learn more <span aria-hidden="true">→</span>
-                      </a>
-                    </div>
-                  </div>
-                  <div className="mt-16 flow-root sm:mt-24">
-                    <div className="flex justify-center align-middle">
-                      <Code />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-x-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-                aria-hidden="true"
-              >
-                <div
-                  className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-                  style={{
-                    clipPath:
-                      'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex items-start justify-end lg:order-first">
-            <img
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-              alt="Product screenshot"
-              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-              width={2432}
-              height={1442}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
 export function Hero() {
   return (
     <div className="overflow-x-clip overflow-y-visible bg-white dark:bg-gray-900 pb-24 sm:pb-32 w-screen">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 align-middle justify-end xl:gap-x-40">
+        <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none align-middle justify-end">
           <div>
             <div className="relative isolate pt-14">
               <div
@@ -229,13 +150,12 @@ export function Hero() {
               </div>
               <div className="pt-24 sm:pt-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                  <div className="mx-auto max-w-2xl text-center">
+                  <div className="mx-auto max-w-5xl text-center">
                     <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl break-words overflow-hidden dark:text-white">
-                      Models to help detect NSFW images and text
+                      AI models to detect hate speech, adult images and internet filtering.
                     </h1>
-                    <p className="mt-6 text-lg leading-8 text-gray-600 break-words overflow-hidden dark:text-white">
-                      Use these models that can run on the browser today! It is
-                      as simple as 10 lines of code.
+                    <p className="mt-6 text-lg leading-8 text-gray-600 break-words overflow-hidden dark:text-slate-400">
+                      A first class framework designed to run in the browser or as an API. Our models are under <span className={"font-mono font-medium text-sky-500 dark:text-sky-400"}>15MB in size</span>.
                     </p>
                     <div className="mt-10 flex items-center justify-center gap-x-6">
                       <Link
@@ -275,24 +195,24 @@ export function Hero() {
           </div>
           <div className="relative overflow-hidden pt-16 lg:hidden">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
-              <img
-                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-                alt="App screenshot"
-                className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-white/10"
-                width={2432}
-                height={1442}
-              />
+              {/*<img*/}
+              {/*  src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"*/}
+              {/*  alt="App screenshot"*/}
+              {/*  className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-white/10"*/}
+              {/*  width={2432}*/}
+              {/*  height={1442}*/}
+              {/*/>*/}
             </div>
           </div>
           <div className="h-full m-auto align-middle lg:justify-end hidden lg:flex">
             <div className="m-auto">
-              <img
-                src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-                alt="Product screenshot"
-                className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-white/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-                width={2432}
-                height={1442}
-              />
+              {/*<img*/}
+              {/*  src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"*/}
+              {/*  alt="Product screenshot"*/}
+              {/*  className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-white/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"*/}
+              {/*  width={2432}*/}
+              {/*  height={1442}*/}
+              {/*/>*/}
             </div>
           </div>
         </div>
