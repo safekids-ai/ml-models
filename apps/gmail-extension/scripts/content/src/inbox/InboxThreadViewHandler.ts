f3f22621d0a2a15e4b2f82fe7788b436a820c0e4
Abbas Valliani <abbasvalliani@gmail.com>
2024-01-10 10:03:44 -0500

    Thread Id Async error

--- apps/gmail-extension/scripts/content/src/inbox/InboxThreadViewHandler.ts
+++ apps/gmail-extension/scripts/content/src/inbox/InboxThreadViewHandler.ts
@@ -8,43 +8,43 @@ import {EmailMessage} from "../../../common/types/EmailMessage";
 
 export class InboxThreadViewHandler {
 
-    constructor(private readonly backgroundCommunicator: InboxBackgroundCommunicator,
-                private readonly eventHandler: IInboxEventHandler) {
-        this.backgroundCommunicator = backgroundCommunicator;
+  constructor(private readonly backgroundCommunicator: InboxBackgroundCommunicator,
+              private readonly eventHandler: IInboxEventHandler) {
+    this.backgroundCommunicator = backgroundCommunicator;
+  }
+
+  async onInBoxSubject(threadRowView: ThreadRowView): Promise<void> {
+    //const body = threadRowView.getElement().textContent;
+    const subject = threadRowView.getSubject();
+    const threadId = await threadRowView.getThreadIDAsync();
+
+    const myself = this;
+    console.log("Inbox message. id:" + threadId + " subject:" + subject);
+    const emailEvent = new EmailEvent(
+      EventType.EMAIL_THREAD_READ_EVENT,
+      new EmailMessage(null, null, subject, null, threadId));
+
+    const toxic = await this.backgroundCommunicator.handleMLEmailEvent(emailEvent);
+    if (toxic) {
+      if (!threadRowView.destroyed) {
+        threadRowView.addLabel(threadUnkindMessageCSS);
+      }
     }
 
-    async onInBoxSubject(threadRowView: ThreadRowView): Promise<void> {
-        //const body = threadRowView.getElement().textContent;
-        const subject = threadRowView.getSubject();
-        const threadId = threadRowView.getThreadID();
+    //if no thread id is available, not point in logging it
+    if (!threadId) {
+      return;
+    }
 
-        const myself = this;
-        console.log("Inbox message. id:" + threadId + " subject:" + subject);
-        const emailEvent = new EmailEvent(
-            EventType.EMAIL_THREAD_READ_EVENT,
-            new EmailMessage(null, null, subject, null, threadId));
-
-        const toxic = await this.backgroundCommunicator.handleMLEmailEvent(emailEvent);
-        if (toxic) {
-            if (!threadRowView.destroyed) {
-                threadRowView.addLabel(threadUnkindMessageCSS);
-            }
-        }
-
-        //if no thread id is available, not point in logging it
-        if (!threadId) {
-            return;
-        }
-
-        //send only if there is a toxic event
-        if (toxic) {
-            const emailNotificationEvent : EmailNotificationEvent = {
-                eventTypeId: EventType.EMAIL_THREAD_READ_EVENT,
-                threadId: threadId,
-                messageId: null,
-                mlFlag: toxic ? Flag.UNKIND : Flag.KIND
-            }
-            this.eventHandler.notifyActivity(emailNotificationEvent)
-        }
-    };
+    //send only if there is a toxic event
+    if (toxic) {
+      const emailNotificationEvent: EmailNotificationEvent = {
+        eventTypeId: EventType.EMAIL_THREAD_READ_EVENT,
+        threadId: threadId,
+        messageId: null,
+        mlFlag: toxic ? Flag.UNKIND : Flag.KIND
+      }
+      this.eventHandler.notifyActivity(emailNotificationEvent)
+    }
+  };
 }
