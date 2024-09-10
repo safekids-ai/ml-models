2f0c7bc58f0c247859963821a50d1e2e51e79980
Abbas Valliani <abbasvalliani@gmail.com>
2024-09-10 13:13:07 -0400

    Added extensions libs

---
+++ libs/extension-storage/src/index.ts
@@ -0,0 +1,7 @@
+import { createStorage } from './lib/base';
+import { exampleThemeStorage } from './lib/exampleThemeStorage';
+import { SessionAccessLevelEnum, StorageEnum } from './lib/enums';
+import type { BaseStorage } from './lib/types';
+
+export { exampleThemeStorage, createStorage, StorageEnum, SessionAccessLevelEnum };
+export type { BaseStorage };
