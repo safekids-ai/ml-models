48c1a246f8e3a1cfc05ce398fe900ead7a4effdc
Zohran valliani <zohranvalliani@Zohrans-MacBook-Air-2.local>
2025-01-05 12:55:11 -0500

    Added tooltip/hover-panel for unkind messages

--- apps/gmail-extension/main/content/src/inbox/InBoxComposeViewHandler.ts
+++ apps/gmail-extension/main/content/src/inbox/InBoxComposeViewHandler.ts
@@ -1,14 +1,18 @@
-import {ComposeView} from "@inboxsdk/core";
-import {EmailNotificationEvent, EventType, Flag} from "@shared/types/events.types";
-import {PrrUserAction} from "@shared/enum/prrAction";
-import {EmailMessage} from "@shared/types/EmailMessage";
-import {ComposeOption} from "@src/InBoxSdkHandler";
-import {InboxBackgroundCommunicator} from "@src/inbox/InboxBackgroundCommunicator";
-import {IInboxEventHandler} from "@src/inbox/InBoxEventHandler";
-import {IdleTimer} from "@src/inbox/IdleTimer";
-import {IInboxDialogs} from "@src/inbox/InboxDialogs";
-import {DOMUtils} from "@src/utils/DOMUtils";
-import {v4 as uuidv4} from 'uuid';
+import { ComposeView } from '@inboxsdk/core';
+import {
+  EmailNotificationEvent,
+  EventType,
+  Flag,
+} from '@shared/types/events.types';
+import { PrrUserAction } from '@shared/enum/prrAction';
+import { EmailMessage } from '@shared/types/EmailMessage';
+import { ComposeOption } from '@src/InBoxSdkHandler';
+import { InboxBackgroundCommunicator } from '@src/inbox/InboxBackgroundCommunicator';
+import { IInboxEventHandler } from '@src/inbox/InBoxEventHandler';
+import { IdleTimer } from '@src/inbox/IdleTimer';
+import { IInboxDialogs } from '@src/inbox/InboxDialogs';
+import { DOMUtils } from '@src/utils/DOMUtils';
+import { v4 as uuidv4 } from 'uuid';
 
 export class InBoxComposeViewHandler {
   set modelRunner(value: InboxBackgroundCommunicator) {
@@ -16,7 +20,7 @@ export class InBoxComposeViewHandler {
   }
 
   private maxForcedRetries = 0;
-  private _threadId = "sf-" + uuidv4();
+  private _threadId = 'sf-' + uuidv4();
   private messsageClean: boolean = false;
   private _attemptsToChangeMessage: number = 0;
   private _modelRunner: InboxBackgroundCommunicator;
@@ -27,7 +31,11 @@ export class InBoxComposeViewHandler {
   private oldComposeText: string;
   private composeView: ComposeView | null;
 
-  constructor(modelRunner: InboxBackgroundCommunicator, prrDialog: IInboxDialogs, eventHandler: IInboxEventHandler) {
+  constructor(
+    modelRunner: InboxBackgroundCommunicator,
+    prrDialog: IInboxDialogs,
+    eventHandler: IInboxEventHandler,
+  ) {
     this._modelRunner = modelRunner;
     this.eventHandler = eventHandler;
     this.prrDialog = prrDialog;
@@ -53,14 +61,14 @@ export class InBoxComposeViewHandler {
       //@typescript-eslint/no-unused-vars
       onClick() {
         myself.highlightToxicText(composeView);
-      }
+      },
     });
 
-    composeView.on("presending", function (event) {
+    composeView.on('presending', function (event) {
       myself.onInBoxCompose(composeView, event);
     });
 
-    composeView.on("bodyChanged", function () {
+    composeView.on('bodyChanged', function () {
       if (myself.composeView == null) {
         myself.composeView = composeView;
       }
@@ -68,7 +76,7 @@ export class InBoxComposeViewHandler {
       myself.onInBoxComposeBodyChange(composeView);
     });
 
-    composeView.on("destroy", function () {
+    composeView.on('destroy', function () {
       myself.composeView = null;
 
       //reset on destroy
@@ -78,7 +86,6 @@ export class InBoxComposeViewHandler {
         clearInterval(myself.interval);
       }
     });
-
   }
 
   onInBoxComposeBodyChange(composeView: ComposeView): void {
@@ -88,7 +95,7 @@ export class InBoxComposeViewHandler {
   resetParams() {
     this.messsageClean = false;
     this._attemptsToChangeMessage = 0;
-    this._threadId = "sf-" + uuidv4();
+    this._threadId = 'sf-' + uuidv4();
   }
 
   async onInBoxCompose(composeView: ComposeView, event: any): Promise<void> {
@@ -101,22 +108,31 @@ export class InBoxComposeViewHandler {
       return;
     }
 
-    const message = {subject: composeView.getSubject(), body: composeView.getTextContent()};
-    const toxic = await this._modelRunner.isToxicAsync(null, null, message.subject + "." + message.body, 300, true);
+    const message = {
+      subject: composeView.getSubject(),
+      body: composeView.getTextContent(),
+    };
+    const toxic = await this._modelRunner.isToxicAsync(
+      null,
+      null,
+      message.subject + '.' + message.body,
+      300,
+      true,
+    );
 
     if (!toxic) {
-      console.log("Clean Compose:" + message.subject + "." + message.body);
+      console.log('Clean Compose:' + message.subject + '.' + message.body);
       if (composeView.destroyed) {
         this.resetParams();
         return;
       }
 
       const emailEvent: EmailNotificationEvent = {
-        eventTypeId: EventType.EMAIL_SEND_EVENT
-      }
-      emailEvent.mlFlag = Flag.KIND
+        eventTypeId: EventType.EMAIL_SEND_EVENT,
+      };
+      emailEvent.mlFlag = Flag.KIND;
       emailEvent.threadId = this._threadId;
-      this.eventHandler.notifyActivity(emailEvent)
+      this.eventHandler.notifyActivity(emailEvent);
       //reset params
       //this.resetParams();
       this.messsageClean = true;
@@ -127,24 +143,31 @@ export class InBoxComposeViewHandler {
         this.resetParams();
         return;
       }
-      event.mlFlag = Flag.UNKIND
-      const forceToChangeMessage = (this._attemptsToChangeMessage < this.maxForcedRetries);
-      const prrResponse = forceToChangeMessage ? await this.prrDialog.doPRRComposeForceChange() :
-        await this.prrDialog.doPRRComposeSendItAnyway();
+      event.mlFlag = Flag.UNKIND;
+      const forceToChangeMessage =
+        this._attemptsToChangeMessage < this.maxForcedRetries;
+      const prrResponse = forceToChangeMessage
+        ? await this.prrDialog.doPRRComposeForceChange()
+        : await this.prrDialog.doPRRComposeSendItAnyway();
       this._attemptsToChangeMessage++;
 
       const emailEvent: EmailNotificationEvent = {
         eventTypeId: EventType.EMAIL_SEND_EVENT,
         prrTriggered: true,
         threadId: this._threadId,
-        mlFlag: Flag.UNKIND
-      }
+        mlFlag: Flag.UNKIND,
+      };
       switch (prrResponse) {
         case ComposeOption.MISTAKE: {
           emailEvent.prrMessage = "Yes, it's fine";
           emailEvent.userFlag = Flag.KIND;
           emailEvent.prrAction = PrrUserAction.COMPOSE_ACTION_YES_ITS_FINE;
-          emailEvent.emailMessage = new EmailMessage(null, null, message.subject, message.body);
+          emailEvent.emailMessage = new EmailMessage(
+            null,
+            null,
+            message.subject,
+            message.body,
+          );
           this.eventHandler.notifyActivity(emailEvent);
           this.messsageClean = true;
           composeView.send();
@@ -152,7 +175,7 @@ export class InBoxComposeViewHandler {
         }
         case ComposeOption.NO_MISTAKE: {
           emailEvent.prrAction = PrrUserAction.COMPOSE_ACTION_NO_TRY_AGAIN;
-          emailEvent.prrMessage = "No, let me try again";
+          emailEvent.prrMessage = 'No, let me try again';
           if (!forceToChangeMessage) {
             emailEvent.userFlag = Flag.UNKIND;
           }
@@ -175,13 +198,18 @@ export class InBoxComposeViewHandler {
       //console.log("Node:" + node.nodeName + "->" + node.nodeType);
       if (node.nodeType == Node.TEXT_NODE) {
         const plainText = node.textContent;
-        const toxic = await this._modelRunner.isToxicAsync(null, null, plainText, 100, true);
+        const toxic = await this._modelRunner.isToxicAsync(
+          null,
+          null,
+          plainText,
+          100,
+          true,
+        );
         if (toxic) {
           nodesToHighlight.add(node);
         }
       }
     }
-
 
     //if something changed ignore the update
     if (textContent != _composeView.getTextContent()) {
@@ -193,8 +221,8 @@ export class InBoxComposeViewHandler {
       if (node.nodeType == Node.TEXT_NODE) {
         if (node.parentNode instanceof HTMLElement) {
           const htmlNode = node.parentNode as HTMLElement;
-          if (htmlNode.classList.contains("sk_toxic")) {
-            htmlNode.classList.remove("sk_toxic");
+          if (htmlNode.classList.contains('sk_toxic')) {
+            htmlNode.classList.remove('sk_toxic');
           }
         }
       }
@@ -207,15 +235,156 @@ export class InBoxComposeViewHandler {
         const htmlElement = node.parentNode as HTMLElement;
         //console.log("Parent node is " + htmlElement.tagName);
         //did we insert this ourselves
-        if (htmlElement.classList.contains("sk_modify")) {
-          htmlElement.classList.add("sk_toxic");
+        let span: HTMLSpanElement | undefined;
+        if (htmlElement.classList.contains('sk_modify')) {
+          htmlElement.classList.add('sk_toxic');
+          span = htmlElement;
         } else {
           //create a new span
-          const span = document.createElement("span");
-          span.classList.add("sk_toxic", "sk_modify");
+          span = document.createElement('span');
+          span.classList.add('sk_toxic', 'sk_modify');
           node.parentNode.insertBefore(span, node);
           span.appendChild(node);
         }
+        let hasCreatedTooltip = false;
+        let isMouseOverTooltip = false;
+        let isMouseOverSpan = true;
+
+        span.addEventListener('mouseover', () => {
+          isMouseOverSpan = true;
+          if (hasCreatedTooltip) return;
+          if (span.classList.contains('sk_toxic')) {
+            hasCreatedTooltip = true;
+            span.classList.add('span_hover');
+
+            function positionTooltip(div: HTMLDivElement) {
+              const rect = span.getBoundingClientRect();
+              const parentRect = span.parentElement.getBoundingClientRect()
+              const tooltipWidth = 250;
+              const min = parentRect.left
+              const max = parentRect.left + parentRect.width - tooltipWidth;
+
+              let positionLeft = rect.left + rect.width / 2 - tooltipWidth / 2;
+              let positionTop = rect.top + rect.height + 10;
+
+              if (positionLeft < min) {
+                positionLeft = min;
+              }
+
+              if (positionLeft > max) {
+                positionLeft = max;
+              }
+
+              div.style.left = `${positionLeft}px`;
+              div.style.top = `${positionTop}px`;
+              div.style.width = `${tooltipWidth}px`;
+            }
+
+            function createButtonGroup(parentDiv: HTMLDivElement) {
+              const noButton = document.createElement('button');
+              const yesButton = document.createElement('button');
+              noButton.className = 'tooltipBtn';
+              noButton.textContent = `✓ I'll fix it`;
+
+              noButton.onclick = () => {
+                document.body.removeChild(parentDiv);
+                span.classList.remove('span_hover');
+                isMouseOverSpan = false;
+                isMouseOverTooltip = false;
+                hasCreatedTooltip = false;
+              };
+
+              yesButton.className = 'tooltipBtn';
+              yesButton.textContent = `🗑️ This text is kind`;
+
+              yesButton.onclick = () => {
+                document.body.removeChild(parentDiv);
+                isMouseOverSpan = false;
+                isMouseOverTooltip = false;
+                hasCreatedTooltip = false;
+
+                const parentElement = span.parentElement;
+                const textContent = span.textContent;
+                const textNode = document.createTextNode(textContent);
+                parentElement.replaceChild(textNode, span);
+                // Make API Call
+              };
+
+              const buttonGroupDiv = document.createElement('div');
+              buttonGroupDiv.className = 'tooltipBtnGroup';
+              buttonGroupDiv.appendChild(noButton);
+              buttonGroupDiv.appendChild(yesButton);
+
+              return buttonGroupDiv;
+            }
+            function createTooltip(): {
+              parentDiv: HTMLDivElement;
+              div: HTMLDivElement;
+            } {
+              const div = document.createElement('div');
+              div.className = 'tooltip';
+              div.innerHTML = `<p>This text doesn't look kind</p>`;
+
+              const parentDiv = document.createElement('div');
+              parentDiv.setAttribute('contenteditable', 'false');
+              parentDiv.className = 'tooltip_container';
+
+              const buttonGroupDiv = createButtonGroup(parentDiv);
+
+              const logo = document.createElement('div');
+              logo.className = 'tooltip_logo';
+              const logoImg = document.createElement('img');
+              logoImg.src = chrome.runtime.getURL('images/logo.svg');
+              logoImg.alt = 'Logo';
+              logo.appendChild(logoImg);
+
+              div.appendChild(buttonGroupDiv);
+              div.appendChild(logo);
+
+              parentDiv.appendChild(div);
+              document.body.appendChild(parentDiv);
+
+              return { parentDiv, div };
+            }
+
+            const { parentDiv, div } = createTooltip();
+            positionTooltip(div);
+
+            function handleResize() {
+              positionTooltip(div);
+            }
+
+            window.addEventListener('resize', handleResize);
+
+            function removeTooltip(parentDiv: HTMLDivElement) {
+              setTimeout(() => {
+                if (!isMouseOverTooltip && !isMouseOverSpan) {
+                  if (!document.body.contains(parentDiv)) return;
+                  document.body.removeChild(parentDiv);
+                  span.classList.remove('span_hover');
+                  isMouseOverSpan = false;
+                  isMouseOverTooltip = false;
+                  hasCreatedTooltip = false;
+                  window.removeEventListener('resize', handleResize);
+                }
+              }, 800);
+            }
+
+            div.addEventListener('mouseleave', () => {
+              isMouseOverTooltip = false;
+              removeTooltip(parentDiv);
+            });
+            div.addEventListener('mouseover', () => {
+              isMouseOverTooltip = true;
+            });
+            span.addEventListener('mouseleave', () => {
+              isMouseOverSpan = false;
+              if (span.classList.contains('sk_toxic')) {
+                removeTooltip(parentDiv);
+              }
+            });
+          }
+        });
       }
     }
   }
